ce504231f86f5ed002b6b6fe46602568ccf1ffa7
Cillian Berragan <cjberragan@gmail.com>
2025-04-02 13:32:35 +0100

    add storage to __init__.py (#21)
    * add initial Storage implementation
    
    * combine json upload and any filetype
    
    * read files async and stream upload, support directories
    
    * gateway_hostname in constructor, remove chunk_size tool, upload data if
    not a file
    
    * fix gateway_url
    
    * add storage to __init__.py
    
    ---------
    
    Signed-off-by: Cillian Berragan <cjberragan@gmail.com>

--- python/thirdweb-ai/src/thirdweb_ai/__init__.py
+++ python/thirdweb-ai/src/thirdweb_ai/__init__.py
@@ -1,6 +1,7 @@
 from .services.engine import Engine
 from .services.insight import Insight
 from .services.nebula import Nebula
+from .services.storage import Storage
 from .tools.tool import Tool
 
-__all__ = ["Engine", "Insight", "Nebula", "Tool"]
+__all__ = ["Engine", "Insight", "Nebula", "Storage", "Tool"]
