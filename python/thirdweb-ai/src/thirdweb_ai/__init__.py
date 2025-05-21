39acb6a618cc0ef96eb061bd1563be12e4946e87
cjber <cjberragan@gmail.com>
2025-05-21 17:48:19 +0100

    fix imports

--- python/thirdweb-ai/src/thirdweb_ai/__init__.py
+++ python/thirdweb-ai/src/thirdweb_ai/__init__.py
@@ -1,9 +1,10 @@
 from ._version import version
 from .services.engine import Engine
+from .services.engine_cloud import EngineCloud
 from .services.insight import Insight
 from .services.nebula import Nebula
 from .services.storage import Storage
 from .tools.tool import Tool
 
 __version__ = version
-__all__ = ["Engine", "Insight", "Nebula", "Storage", "Tool"]
+__all__ = ["Engine", "EngineCloud", "Insight", "Nebula", "Storage", "Tool"]
